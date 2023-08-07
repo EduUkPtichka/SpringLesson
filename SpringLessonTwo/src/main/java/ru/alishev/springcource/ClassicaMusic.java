1d954821f6c879ccee2b54ba4c8744df51abb6be
EduUkPtichka <ukenoveldiyar@gmail.com>
2023-08-07 15:25:22 +0700

    Urok4,try

---
+++ SpringLessonTwo/src/main/java/ru/alishev/springcource/ClassicaMusic.java
@@ -0,0 +1,10 @@
+package ru.alishev.springcource;
+
+public class ClassicaMusic implements Music {
+
+
+    @Override
+    public String getSong() {
+        return "TynTynTyn";
+    }
+}
---
+++ SpringLessonTwo/src/main/java/ru/alishev/springcource/RockMusic.java
@@ -0,0 +1,9 @@
+package ru.alishev.springcource;
+
+public class RockMusic implements Music {
+
+    @Override
+    public String getSong() {
+        return "RARARA";
+    }
+}
